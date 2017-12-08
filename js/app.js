3c6a97d0e856c256fa8041bc06b154f1f99f2b8b
Michael Weber <webermn15@gmail.com>
2017-12-08 16:13:51 -0600

    problem 2 solved

--- js/app.js
+++ js/app.js
@@ -1,13 +1,22 @@
 // 1
 
-var words = ["hello", "what", "is", "up", "dude"]
+// var words = ["hello", "what", "is", "up", "dude"]
 
-function lengths(arr) {
-	let wordLengthArr = [];
-	for (let i = 0; i <= arr.length - 1; i++) {
-		wordLengthArr.push(arr[i].length);
-	}
-	console.log(wordLengthArr);
+// function lengths(arr) {
+// 	let wordLengthArr = [];
+// 	for (let i = 0; i <= arr.length - 1; i++) {
+// 		wordLengthArr.push(arr[i].length);
+// 	}
+// 	console.log(wordLengthArr);
+// }
+
+// console.log(lengths(words));
+
+
+// 2
+
+function transmogrifier(num1, num2, num3) {
+	let numSum = num1 + num2;
+	let solution = Math.pow(numSum, num3);
+	console.log(solution);
 }
-
-console.log(lengths(words));
