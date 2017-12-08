f1c1b9ffb654389f56c4f0d3e55826a6be4c1266
Michael Weber <webermn15@gmail.com>
2017-12-08 16:21:19 -0600

    solved problem 3

--- js/app.js
+++ js/app.js
@@ -28,7 +28,17 @@
 
 // 3
 
-
+function maxOfTwoNumbers(num1, num2) {
+	if (num1 > num2) {
+		console.log(num1+" is larger");
+	}
+	else if (num1 < num2) {
+		console.log(num2+" is larger");
+	}
+	else {
+		console.log("they are equal");
+	}
+}
 
 
 
